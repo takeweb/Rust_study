6b7eaacd7160539cd06d5ac978ab7cbdbad4fa68
Taketomo Oishi <oishi@multisoup.co.jp>
2020-12-04 10:06:56 +0900

    add other all

---
+++ loops/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let a = [10, 20, 30, 40, 50];
+    //let mut index = 0;
+
+    /*while index < 5 {
+        println!("the value is {}", a[index]);
+        index = index + 1;
+    }*/
+
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+}
