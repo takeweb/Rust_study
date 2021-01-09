353250c3df4678a40a92b0141108705a4636d3fc
Taketomo Ohishi <takeweb@mac.com>
2021-01-10 00:34:44 +0900

    20210109対応分

--- mkdir/src/main.rs
+++ mkdir/src/main.rs
@@ -1,12 +1,8 @@
 use std::fs;
-// use std::path::PathBuf;
 use std::path::Path;
 use chrono::{Local, DateTime, Datelike};
-use jpholiday::jpholiday::JPHoliday;
-use jpholiday::chrono::{NaiveDate};
-use std::borrow::Borrow;
-use std::convert::TryFrom;
 use std::env;
+mod date_util;
 
 fn main() {
     let year: i32;
@@ -24,32 +20,15 @@ fn main() {
 
     let dir = format!("./test/{}/{:02}", year, month);
     let path = Path::new(&dir);
-    let jpholiday = JPHoliday::new();
-    let max_day: u32 = TryFrom::try_from(get_days_from_month(year, month)).unwrap();
+    let mut my_holiday = date_util::MyHoliday::new();
+    let max_day: u32 = date_util::get_days_from_ym(year, month);
 
     for i in 1..=max_day {
-        let holiday_flg = jpholiday.is_holiday(NaiveDate::from_ymd(year, month, i).borrow());
-        if !holiday_flg {
+        if my_holiday.is_bizday(year, month, i) {
             let ymd = format!("{}{:02}{:02}", year, month, i);
             let new_path = path.join(ymd);
             fs::create_dir_all(new_path)
                 .expect("error");
         }
     }
-}
-
-fn get_days_from_month(year: i32, month: u32) -> i64 {
-    NaiveDate::from_ymd(
-        match month {
-            12 => year + 1,
-            _ => year,
-        },
-        match month {
-            12 => 1,
-            _ => month + 1,
-        },
-        1,
-    )
-    .signed_duration_since(NaiveDate::from_ymd(year, month, 1))
-    .num_days()
 }
--- oop/src/lib.rs
+++ oop/src/lib.rs
@@ -6,7 +6,7 @@ pub struct AveragedCollection {
 impl AveragedCollection {
     pub fn add(&mut self, value: i32) {
         self.list.push(value);
-        self.update_average():
+        self.update_average();
     }
 
     pub fn remove(&mut self) -> Option<i32> {
@@ -25,7 +25,7 @@ impl AveragedCollection {
     }
 
     fn update_average(&mut self) {
-        let total; i32 = self.list.iter().sum();
+        let total: i32 = self.list.iter().sum();
         self.average = total as f64 / self.list.len() as f64;
     }
 }
